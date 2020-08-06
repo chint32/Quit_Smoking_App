ee556d9e009c380f7819fa5cbe934243faf53704
Cotey Hinton <hinton.c32@gmail.com>
2020-08-05 18:01:46 -0700

    Added find near by smoking cessation services via google maps

--- app/src/main/java/com/chstudios/quitsmokingapp/CessationServicesActivty.kt
+++ app/src/main/java/com/chstudios/quitsmokingapp/CessationServicesActivty.kt
@@ -1,11 +1,23 @@
 package com.chstudios.quitsmokingapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class CessationServicesActivty : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cesation_services_activty)
+
+        val gmmIntentUri = Uri.parse("geo:?q=smoking cessation services")
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        mapIntent.resolveActivity(packageManager)?.let {
+            startActivity(mapIntent)
+        }
     }
+
 }
